d94aaff962938c082a0c60d276b423703c0f1391
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2022-07-18 17:21:14 +0200

    ~ tmp26 - dynamic in fir2ir, TODO: refactor/comments

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/generators/CallAndReferenceGenerator.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/generators/CallAndReferenceGenerator.kt
@@ -421,14 +421,17 @@ class CallAndReferenceGenerator(
             // into a non-fake member. For example, we can
             // resolve into members of `Any`.
             val convertedExplicitReceiver = if (explicitReceiverExpression?.type is IrDynamicType) {
-                qualifiedAccess.convertWithOffsets { startOffset, endOffset ->
-                    val targetType = (firSymbol?.fir as? FirCallableDeclaration)?.dispatchReceiverType?.toIrType()
-                        ?: error("Couldn't get the proper receiver")
-                    IrTypeOperatorCallImpl(
-                        startOffset, endOffset, targetType,
-                        IrTypeOperator.IMPLICIT_DYNAMIC_CAST,
-                        targetType, explicitReceiverExpression,
-                    )
+                val targetType = (firSymbol?.fir as? FirCallableDeclaration)?.dispatchReceiverType?.toIrType()
+                if (targetType != null) {
+                    qualifiedAccess.convertWithOffsets { startOffset, endOffset ->
+                        IrTypeOperatorCallImpl(
+                            startOffset, endOffset, targetType,
+                            IrTypeOperator.IMPLICIT_DYNAMIC_CAST,
+                            targetType, explicitReceiverExpression,
+                        )
+                    }
+                } else {
+                    explicitReceiverExpression
                 }
             } else {
                 explicitReceiverExpression
