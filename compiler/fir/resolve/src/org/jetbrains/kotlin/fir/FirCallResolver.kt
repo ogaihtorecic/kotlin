c5150c39df2f19bbad699182fed648f70b271b5c
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2022-07-19 08:43:51 +0200

    Stop tower resolve in K2 in more similar manner with K1

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/FirCallResolver.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/FirCallResolver.kt
@@ -180,7 +180,7 @@ class FirCallResolver(
             }
 
             // We want to get candidates at all tower levels.
-            override fun shouldStopAtTheLevel(group: TowerGroup): Boolean = false
+            override fun shouldStopAtTheGroup(group: TowerGroup): Boolean = false
 
             val allCandidates: List<Candidate>
                 get() = allCandidatesSet.toList()
